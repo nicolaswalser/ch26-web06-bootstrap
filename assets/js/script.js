3277903d78b7743e668ac6a0c51c9de8b207b8fc
Nicolas <walsergomez@gmail.com>
2023-04-25 17:54:35 -0600

    ci: calculator created

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,26 @@ function promptAndChange() {
   const newName = prompt("Enter your name:");
   document.getElementById("yourName").innerHTML = "Your name is: " + newName;
 }
+
+function performOperation(operation) {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  let result;
+  switch (operation) {
+    case "add":
+      result = num1 + num2;
+      break;
+    case "subtract":
+      result = num1 - num2;
+      break;
+    case "multiply":
+      result = num1 * num2;
+      break;
+    case "divide":
+      result = num1 / num2;
+      break;
+    default:
+      result = "Invalid operation";
+  }
+  document.getElementById("result").value = result;
+}
